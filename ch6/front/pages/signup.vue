21db1d151d8edb9f823ba87897b0b3b205d3288e
Nam-SH <gtsmell@gmail.com>
2020-01-20 22:29:39 +0900

    20200120 | Update

--- ch6/front/pages/signup.vue
+++ ch6/front/pages/signup.vue
@@ -49,13 +49,6 @@
         </v-container>
       </v-card>
     </v-container>
-    
-    <form action="/api/auth/login" method="POST">
-        <input type="text" name="username" placeholder="username">
-        <input type="password" name="password" placeholder="password">
-        <button type="submit">submit</button>
-    </form>
-    
   </div>
 </template>
 
