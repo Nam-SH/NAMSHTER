6e8f84d05c081869dc2d5a88ff3764354c21d6c1
Nam-SH <gtsmell@gmail.com>
2020-01-24 12:44:41 +0900

    20200124 | Update

--- ch6/back/app.js
+++ ch6/back/app.js
@@ -81,9 +81,6 @@ app.use('/post', postRouter)
 app.use('/posts', postsRouter)
 app.use('/hashtag', hashtagRouter)
 
-
-
 app.listen(prod ? process.env.PORT : 3085, () => {
   console.log(`백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동 중...`);
-  
 })
