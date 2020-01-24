20f94f3b1dbfa9a82a5c3183582cb6d072edcbff
Nam-SH <gtsmell@gmail.com>
2020-01-24 12:11:55 +0900

    20200124 | Update

--- ch6/back/app.js
+++ ch6/back/app.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const db = require('./models');
 const app = express();
 
+const hpp = require('hpp')
+const helmet = require('helmet')
+
 const session = require('express-session');
 const passport = require('passport');
 const passportConfig = require('./passport');
@@ -15,16 +18,37 @@ const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
 const hashtagRouter = require('./routes/hashtag');
 
+const dotenv = require('dotenv')
+
+// 배포용인지 확인
+const prod = process.env.NODE_ENV === 'production';
+
 // db 강제로 덮어씌우기
 // db.sequelize.sync({force: true})
 db.sequelize.sync();
 passportConfig();
 
-app.use(morgan('dev'))
-app.use(cors({
-  origin: 'http://localhost:3081',
-  credentials: true,
-}));
+dotenv.config()
+
+if (prod) {
+  // 배포용
+  app.use(helmet());
+  app.use(hpp());
+  app.use(morgan('combined'));
+  app.use(cors({
+    origin: 'http://namshter.com',
+    credentials: true,
+  }));
+}
+else {
+  app.use(morgan('dev'))
+  app.use(cors({
+    origin: 'http://localhost:3081',
+    credentials: true,
+  }));
+}
+
+
 
 // 정적파일 사용 => 서버에서 정확한 위치를 보이지 않이 위해, 단순히 / 로 요청을 받게 한다.
 app.use('/', express.static('uploads'));
@@ -32,14 +56,15 @@ app.use('/', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookie('cookiesecret'))
+app.use(cookie(process.env.COOKIE_SECRET))
 app.use(session({
   resave: false,
   saveUninitialized: false,
-  secret: 'cookiesecret',
+  secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
+    domain: prod && '.manshter.com'
   }
 }));
 
@@ -58,13 +83,7 @@ app.use('/hashtag', hashtagRouter)
 
 
 
-app.listen(3085, () => {
-  console.log('\u001b[36m', '     ╭----------------------------------------------------------------╮');
-  console.log('\u001b[36m', '     |                                                                 |');
-  console.log('\u001b[35m', `     |              백엔드 서버 ${3085}번 프로에서 작동중...              |`);
-  console.log('\u001b[36m', '     |                                                                 |');
-  console.log('\u001b[31m', `     |                   http://localhost:${3085}/                        |`);
-  console.log('\u001b[36m', '     |                                                                 |');
-  console.log('\u001b[36m', '     ╰----------------------------------------------------------------╯');
-  console.log('\u001b[0m');
+app.listen(prod ? process.env.PORT : 3085, () => {
+  console.log(`백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동 중...`);
+  
 })
