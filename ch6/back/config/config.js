85c1e06bd430195b78dbb80790b0c28753399b85
Nam-SH <gtsmell@gmail.com>
2020-01-24 12:36:13 +0900

    20200124 | Update

--- ch6/back/config/config.js
+++ ch6/back/config/config.js
@@ -7,21 +7,21 @@ module.exports =
   "development": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "vuenodebird_ch6_D",
+    "database": "namshter",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "vuenodebird_ch6_T",
+    "database": "namshter",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "vuenodebird_ch6_P",
+    "database": "namshter",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
