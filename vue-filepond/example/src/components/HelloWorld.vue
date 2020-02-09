ca16e081caaa16835505251361d4c42896d2027a
Nam-SH <gtsmell@gmail.com>
2020-02-09 20:54:28 +0900

    20200209 | Update: filepond start

---
+++ vue-filepond/example/src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div id="component" style="width: 300px; height: 400px;">
+      <file-pond
+        name="image"
+        ref="pond"
+        label-idle="Drop files here..."
+        allow-multiple="true"
+        v-bind:server="myServer"
+        v-bind:files="myFiles"
+        v-on:init="handleFilePondInit"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// Create component
+const FilePond = vueFilePond(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview
+);
+
+export default {
+  data: function() {
+    return {
+      // fake server to simulate loading a 'local' server file and processing a file
+      myServer: {
+        process: (fieldName, file, metadata, load) => {
+          // simulates uploading a file
+          console.log(file.name);
+
+          setTimeout(() => {
+            load(Date.now());
+          }, 1500);
+        },
+        load: (source, load) => {
+          // simulates loading a file from the server
+          fetch(source)
+            .then(res => res.blob())
+            .then(load);
+        }
+      },
+      myFiles: [
+        {
+          source: "photo.jpeg",
+          options: {
+            type: "local"
+          }
+        }
+      ],
+      filename: ""
+    };
+  },
+  methods: {
+    handleFilePondInit: function() {
+      // FilePond instance methods are available on `this.$refs.pond`
+
+      /* eslint-disable */
+      console.log("FilePond has initialized");
+    }
+  },
+  components: {
+    FilePond
+  }
+};
+</script>
